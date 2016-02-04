f8b5de576dbe6e8ef0f338c44b08cce1c70f5f54
Joe Pinsonault <joe.pinsonault@gmail.com>
2016-02-04 15:40:20 -0800

    (QA-2356) Convert readme to rst
    Pypi wants .rst files, and github supports them

--- setup.py
+++ setup.py
@@ -9,17 +9,22 @@ try:
 except ImportError:
   from distutils.core import setup
 
+from os import path
 from vmpooler_client.version import version
+
+def load_readme(readme_filename):
+    return open(path.join(path.dirname(__file__), readme_filename)).read()
 
 #===================================================================================================
 # Main
 #===================================================================================================
 setup(name='vmpooler-client',
-       version=version,
-       description='Manage resources in the vmpooler service from the command-line.',
-       author='Ryan Gard',
-       author_email='ryan.gard@puppetlabs.com',
-       url='https://github.com/puppetlabs/vmpooler-client',
-       packages=['vmpooler_client', 'vmpooler_client.commands'],
-       scripts=['vmpooler_client_app.py']
+      version=version,
+      description='Manage resources in the vmpooler service from the command-line.',
+      long_description=load_readme("README.rst"),
+      author='Ryan Gard',
+      author_email='ryan.gard@puppetlabs.com',
+      url='https://github.com/puppetlabs/vmpooler-client',
+      packages=['vmpooler_client', 'vmpooler_client.commands'],
+      scripts=['vmpooler_client_app.py']
      )
